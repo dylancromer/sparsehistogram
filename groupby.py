afd5a20f6f9e825b84d8bb6210c8972bd9a9a236
DylanC <dylan@dylancromer.com>
2018-04-08 22:19:39 -0400

    Functionalized groupby.py

--- groupby.py
+++ groupby.py
@@ -1,13 +1,11 @@
 import numpy as np
 from importgals import import_csv_nonp
-import time
 
 
 gals  = import_csv_nonp('testpoints.csv')
 gals = [[float(coord) for coord in gal] for gal in gals]
 
-def groupbybin(points, res):
-    start = time.time()
+def groupbybin(points, res, weights=True):
     nbins = np.ceil(10/res)
 
     def binindex(point):
@@ -17,19 +15,17 @@ def groupbybin(points, res):
         return bi
 
     from collections import defaultdict
-    pointlist = [(binindex(x), x[3]) for x in points]
+    if weights:
+        pointlist = [(binindex(x), x[3]) for x in points]
+    else:
+        pointlist = [(binindex(x), 1) for x in points]
+
     pointdict = defaultdict(list)
     for k,v in pointlist:
         pointdict[k].append(v)
-    chk1 = time.time()
-    print('Dictionary create time: ' + str(chk1 - start))
 
     for key,val in pointdict.items():
         val = sum(val)
         pointdict.update({key:val})
-    chk2 = time.time()
-    print('Dictionary update time: ' + str(chk2 - chk1))
-    print('Total elapsed time: ' + str(chk2 - start))
     return pointdict
 
-groupbybin(gals, 1)
