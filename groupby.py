5b9a731f417cd18a63756b6c6bd34e5bed99f31e
DylanC <dylan@dylancromer.com>
2018-04-09 13:53:59 -0400

    Fixed some issues

--- groupby.py
+++ groupby.py
@@ -1,21 +1,18 @@
 import numpy as np
 from importgals import import_csv_nonp
-
+from collections import defaultdict
 
 if __name__ == "__main__":
     gals  = import_csv_nonp('testpoints.csv')
     gals = [[float(coord) for coord in gal] for gal in gals]
 
 def groupbybin(points, res, weights=True):
-    nbins = np.ceil(10/res)
-
     def binindex(point):
         point = point[:3]
         bi = [int(x//res) for x in point]
         bi = tuple(bi)
         return bi
 
-    from collections import defaultdict
     if weights:
         pointlist = [(binindex(x), x[3]) for x in points]
     else:
