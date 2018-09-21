7222df995d4267f712788d0a4586cf35a1087060
Dylan@ArchLutece <dylanmattc@gmail.com>
2018-09-20 22:42:20 -0400

    Updated to latest version of function, cleaned up

--- groupby.py
+++ groupby.py
@@ -1,22 +1,29 @@
-import numpy as np
-from importgals import import_csv_nonp
 from collections import defaultdict
 
-if __name__ == "__main__":
-    gals  = import_csv_nonp('testpoints.csv')
-    gals = [[float(coord) for coord in gal] for gal in gals]
-
-def groupbybin(points, res, weights=True):
-    def binindex(point):
+"""Accepts a python list of 3D spatial points, e.g. [[x1,y1,z1],...], optionally with weights e.g. [[x1,x2,x3,w1],...], and returns the sparse histogram (i.e. no empty bins) with bins of resolution (spacing) given by res."""
+def sparse_hist(points, res, weights=True):
+    def _binindex(point):
         point = point[:3]
         bi = [int(x//res) for x in point]
         bi = tuple(bi)
         return bi
 
-    if weights:
-        pointlist = [(binindex(x), x[3]) for x in points]
+    def _bincenter(point):
+        point = point[:3]
+        bc = [(x//res+0.5)*res for x in point]
+        bc = tuple(bc)
+        return bc
+
+    if not bin_index:
+        if weights:
+            pointlist = [(_bincenter(x), x[3]) for x in points]
+        else:
+            pointlist = [(_bincenter(x), 1) for x in points]
     else:
-        pointlist = [(binindex(x), 1) for x in points]
+        if weights:
+            pointlist = [(_binindex(x), x[3]) for x in points]
+        else:
+            pointlist = [(_binindex(x), 1) for x in points]
 
     pointdict = defaultdict(list)
     for k,v in pointlist:
