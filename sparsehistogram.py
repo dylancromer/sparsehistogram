6f112389c5162002326a700b874e83fa27898ed9
dylancromer <dylanmattc@gmail.com>
2018-12-08 16:57:48 -0500

    Made more pep8 compliant

--- sparsehistogram.py
+++ sparsehistogram.py
@@ -1,38 +1,40 @@
 from collections import defaultdict
 
-"""Accepts a python list of 3D spatial points, e.g. [[x1,y1,z1],...], optionally with weights e.g. [[x1,x2,x3,w1],...], and returns the sparse histogram (i.e. no empty bins) with bins of resolution (spacing) given by res.
-The weights option allows you to chose to histogram over counts instead of weights (equivalent to all weights being 1).
-The bin_index option lets you return the points with their bin indices (the integers representing how many bins in each direction to walk to find the specified bin) rather than centerpoint coordinates."""
-def sparse_hist(points, res, weights=True, bin_index=False):
-    def _binindex(point):
-        point = point[:3]
-        bi = [int(x//res) for x in point]
-        bi = tuple(bi)
-        return bi
+def sparse_hist(coords, resolution, use_weights, return_bin_index):
+    """
+    Accepts a python list of 3D spatial points, e.g. [[x1,y1,z1],...], optionally with weights e.g. [[x1,x2,x3,w1],...], and returns the sparse histogram (i.e. no empty bins) with bins of resolution (spacing) given by resolution.
+    The weights option allows you to chose to histogram over counts instead of weights (equivalent to all weights being 1).
+    The bin_index option lets you return the points with their bin indices (the integers representing how many bins in each direction to walk to find the specified bin) rather than centerpoint coordinates.
+    """
 
-    def _bincenter(point):
-        point = point[:3]
-        bc = [(x//res+0.5)*res for x in point]
-        bc = tuple(bc)
-        return bc
+    def _binindex(coord):
+        coord = coord[:3]
+        bin_index = [int(x//resolution) for x in coord]
+        bin_index = tuple(bin_index)
+        return bin_index
 
-    if not bin_index:
-        if weights:
-            pointlist = [(_bincenter(x), x[3]) for x in points]
-        else:
-            pointlist = [(_bincenter(x), 1) for x in points]
+    def _bincenter(coord):
+        coord = coord[:3]
+        bin_center = [(x//resolution+0.5)*resolution for x in coord]
+        bin_center = tuple(bin_center)
+        return bin_center
+
+    if return_bin_index:
+        binning_function = _binindex
     else:
-        if weights:
-            pointlist = [(_binindex(x), x[3]) for x in points]
-        else:
-            pointlist = [(_binindex(x), 1) for x in points]
+        binning_function = _bincenter
 
-    pointdict = defaultdict(list)
-    for k,v in pointlist:
-        pointdict[k].append(v)
+    if use_weights:
+        coord_list = [(binning_function(x), x[3]) for x in coords]
+    else:
+        coord_list = [(binning_function(x), 1) for x in coords]
 
-    for key,val in pointdict.items():
-        val = sum(val)
-        pointdict.update({key:val})
-    return pointdict
+    binned_coords = defaultdict(list)
+    for coord, weight in coord_list:
+        binned_coords[coord].append(weight)
 
+    for coord, weight in binned_coords.items():
+        weight = sum(weight)
+        binned_coords.update({coord:weight})
+
+    return binned_coords
